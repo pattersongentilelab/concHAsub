c86142d9a2aa932a7362c972e3a0c7418d5e6e8c
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2023-04-07 16:57:19 -0400

    blood biomarker code

---
+++ code/Blood biomarker/compileHAsub_BBdata.m
@@ -0,0 +1,6 @@
+% Organize VEP subject data and combine with VEP matlab files
+
+data_path = getpref('concHAsubDataPath','MindsMatter_DataPath');
+
+load([data_path '/blood biomarker/blood_biomarker_022023.mat'])
+

eb8f992b66e27c1fad1179cad263f51c5e36d3d5
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2023-04-08 17:07:08 -0400

    organize VEP and blood biomarker data

--- code/Blood biomarker/compileHAsub_BBdata.m
+++ code/Blood biomarker/compileHAsub_BBdata.m
@@ -1,6 +1,98 @@
 % Organize VEP subject data and combine with VEP matlab files
 
-data_path = getpref('concHAsubDataPath','MindsMatter_DataPath');
+data_path = getpref('concHAsub','concHAsubDataPath');
 
 load([data_path '/blood biomarker/blood_biomarker_022023.mat'])
 
+% Select mean concentration for each of the biomarkers
+NFL_GFAP = table(GFAP_NFL_0223.VarName1,GFAP_NFL_0223.MeanConc,GFAP_NFL_0223.MeanConc1,'VariableNames',{'ID','NFL','GFAP'});
+IL6_IL10 = table(IL6_IL10_0223.SubjectID,IL6_IL10_0223.VarName11,IL6_IL10_0223.VarName14,'VariableNames',{'ID','IL6','IL10'});
+TNFa = table(TNFa_0223.SubjectID,TNFa_0223.VarName10,'VariableNames',{'ID','TNFa'});
+
+% Select and organize hyperacute sample
+NFL_GFAP_acute = NFL_GFAP(contains(NFL_GFAP.ID,'ACE')==1,:);
+IL6_IL10_acute = IL6_IL10(contains(IL6_IL10.ID,'ACE')==1,:);
+TNFa_acute = TNFa(contains(TNFa.ID,'ACE')==1,:);
+TNFa_acute.ID = replace(TNFa_acute.ID,'ACED077','ACED057'); % Check to make sure this is correct
+temp1 = join(NFL_GFAP_acute,IL6_IL10_acute,'Keys','ID');
+biomarker_acute = join(temp1,TNFa_acute,'Keys','ID');
+
+% Select and organize longitudinal sample
+GN_T1 = NFL_GFAP(contains(NFL_GFAP.ID,'T1')==1,:);
+GN_T1.Properties.VariableNames = {'ID','NFL_T1','GFAP_T1'};
+GN_T1.ID = replace(GN_T1.ID,'T1-B','');
+GN_T2 = NFL_GFAP(contains(NFL_GFAP.ID,'T2')==1,:);
+GN_T2.Properties.VariableNames = {'ID','NFL_T2','GFAP_T2'};
+GN_T2.ID = replace(GN_T2.ID,'T2-B','');
+GN_T3 = NFL_GFAP(contains(NFL_GFAP.ID,'T3')==1,:);
+GN_T3.Properties.VariableNames = {'ID','NFL_T3','GFAP_T3'};
+GN_T3.ID = replace(GN_T3.ID,'T3-B','');
+GN_T3.ID = replace(GN_T3.ID,'-02','-002');
+biomarker_long = join(GN_T1,GN_T2,'Keys','ID');
+biomarker_long = join(biomarker_long,GN_T3,'Keys','ID');
+clear GN_T*
+
+TNF_T1 = TNFa(contains(TNFa.ID,'T1')==1,:);
+TNF_T1.Properties.VariableNames = {'ID','TNFa_T1'};
+TNF_T1.ID = replace(TNF_T1.ID,'T1-B','');
+TNF_T2 = TNFa(contains(TNFa.ID,'T2')==1,:);
+TNF_T2.Properties.VariableNames = {'ID','TNFa_T2'};
+TNF_T2.ID = replace(TNF_T1.ID,'T2-B','');
+TNF_T3 = TNFa(contains(TNFa.ID,'T3')==1,:);
+TNF_T3.Properties.VariableNames = {'ID','TNFa_T3'};
+TNF_T3.ID = replace(TNF_T3.ID,'T3-B','');
+biomarker_long = join(biomarker_long,TNF_T1,'Keys','ID');
+biomarker_long = join(biomarker_long,TNF_T2,'Keys','ID');
+biomarker_long = join(biomarker_long,TNF_T3,'Keys','ID');
+clear TNF_T*
+
+IL_T1 = IL6_IL10(contains(IL6_IL10.ID,'T1')==1,:);
+IL_T1.Properties.VariableNames = {'ID','IL6_T1','IL10_T1'};
+IL_T1.ID = replace(IL_T1.ID,'T1-B','');
+IL_T2 = IL6_IL10(contains(IL6_IL10.ID,'T2')==1,:);
+IL_T2.Properties.VariableNames = {'ID','IL6_T2','IL10_T2'};
+IL_T2.ID = replace(IL_T1.ID,'T2-B','');
+IL_T3 = IL6_IL10(contains(IL6_IL10.ID,'T3')==1,:);
+IL_T3.Properties.VariableNames = {'ID','IL6_T3','IL10_T3'};
+IL_T3.ID = replace(IL_T1.ID,'T3-B','');
+biomarker_long = join(biomarker_long,IL_T1,'Keys','ID');
+biomarker_long = join(biomarker_long,IL_T2,'Keys','ID');
+biomarker_long = join(biomarker_long,IL_T3,'Keys','ID');
+clear IL_T*
+
+
+figure
+subplot(5,1,1)
+title('GFAP')
+hold on
+ax = gca; ax.TickDir = 'out'; ax.Box = 'off'; ax.XLim = [0 5]; ax.XTick = 1:4; ax.XTickLabels = {'acute','T1','T2','T3'};
+plot(1,biomarker_acute.GFAP,'ok','MarkerFaceColor','k')
+plot(2:4,[biomarker_long.GFAP_T1 biomarker_long.GFAP_T2 biomarker_long.GFAP_T3],'--ok','MarkerFaceColor','k')
+
+subplot(5,1,2)
+title('NF-L')
+hold on
+ax = gca; ax.TickDir = 'out'; ax.Box = 'off'; ax.XLim = [0 5]; ax.XTick = 1:4; ax.XTickLabels = {'acute','T1','T2','T3'};
+plot(1,biomarker_acute.NFL,'ok','MarkerFaceColor','k')
+plot(2:4,[biomarker_long.NFL_T1 biomarker_long.NFL_T2 biomarker_long.NFL_T3],'--ok','MarkerFaceColor','k')
+
+subplot(5,1,3)
+title('TNFa')
+hold on
+ax = gca; ax.TickDir = 'out'; ax.Box = 'off'; ax.XLim = [0 5]; ax.XTick = 1:4; ax.XTickLabels = {'acute','T1','T2','T3'};
+plot(1,biomarker_acute.TNFa,'ok','MarkerFaceColor','k')
+plot(2:4,[biomarker_long.TNFa_T1 biomarker_long.TNFa_T2 biomarker_long.TNFa_T3],'--ok','MarkerFaceColor','k')
+
+subplot(5,1,4)
+title('IL-6')
+hold on
+ax = gca; ax.TickDir = 'out'; ax.Box = 'off'; ax.XLim = [0 5]; ax.XTick = 1:4; ax.XTickLabels = {'acute','T1','T2','T3'};
+plot(1,biomarker_acute.IL6,'ok','MarkerFaceColor','k')
+plot(2:4,[biomarker_long.IL6_T1 biomarker_long.IL6_T2 biomarker_long.IL6_T3],'--ok','MarkerFaceColor','k')
+
+subplot(5,1,5)
+title('IL-10')
+hold on
+ax = gca; ax.TickDir = 'out'; ax.Box = 'off'; ax.XLim = [0 5]; ax.XTick = 1:4; ax.XTickLabels = {'acute','T1','T2','T3'};
+plot(1,biomarker_acute.IL10,'ok','MarkerFaceColor','k')
+plot(2:4,[biomarker_long.IL10_T1 biomarker_long.IL6_T2 biomarker_long.IL10_T3],'--ok','MarkerFaceColor','k')
--- code/VEP/compileHAsub_VEPdata.m
+++ code/VEP/compileHAsub_VEPdata.m
@@ -1,77 +1,123 @@
 % Organize VEP subject data and combine with VEP matlab files
 
-data_path = getpref('concHAsubDataPath','MindsMatter_DataPath');
+%% select VEP data from headache substudy to match raw VEP file with HSS ID
+data_path = getpref('concHAsub','concHAsubDataPath');
+filepath = [data_path '/VEP'];
 
-readtable([data_path '/excel.csv'])
+tbl1 = readtable([filepath '/excel_03.21.23.csv']);
+tbl2 = readtable([filepath '/rawdataIDlink_03.21.23.csv']);
 
- %% organize and extract VEP files
-filepath = [data_path '/datapull 032123/export20230321/rdata/'];
+tbl1 = tbl1(contains(tbl1.LastName,'HSS')==1,:);
+tbl2 = tbl2(contains(tbl2.Var2,'HSS')==1,:);
 
-% select VEP data from headache substudy
+%% Compile IDs to match study ID with raw matlab file and create filename
+match_ID = table(tbl1.LastName,tbl1.LastName,zeros(size(tbl1.TestID)),tbl1.TestID,tbl1.LastName,'VariableNames',{'StudyID','TimePoint','SessionID','TrialID','Filename'});
+match_ID.TimePoint(contains(match_ID.TimePoint,'T1')==1) = {'T1'};
+match_ID.TimePoint(contains(match_ID.TimePoint,'T2')==1) = {'T2'};
+match_ID.TimePoint(contains(match_ID.TimePoint,'T3')==1) = {'T3'};
+match_ID.StudyID = erase(match_ID.StudyID,'T1');
+match_ID.StudyID = erase(match_ID.StudyID,'T2');
+match_ID.StudyID = erase(match_ID.StudyID,'T3');
 
-subject_data1 = readtable([data_path '/datapull 032123/excel.csv']);
-subject_data2 = readtable([data_path '/datapull 032123/rawdataIDlink_03.21.23.csv']);
+Participants = unique(match_ID.StudyID);
 
-% Match VEP data to subject
-temp = [VEPsubject_data.recordID VEPsubject_data.PatientID VEPsubject_data.TestID];
-
-for x=1:length(temp)
-    if temp(x,3) <100
-        filename(x,:)=['f00000' num2str(temp(x,2)) '_000000' num2str(temp(x,3)) '.m'];
-    else if temp(x,3) >=100 && temp(x,3)<1000
-        filename(x,:)=['f00000' num2str(temp(x,2)) '_00000' num2str(temp(x,3)) '.m'];
-        else if temp(x,3)>=1000
-                filename(x,:)=['f00000' num2str(temp(x,2)) '_0000' num2str(temp(x,3)) '.m'];
-            end
-        end
+for x = 1:length(Participants)
+    for y = 1:3
+        T = {['T' num2str(y)]};
+        temp = tbl2.Var1(contains(tbl2.Var2,Participants(x))==1 & contains(tbl2.Var2,T)==1);
+        match_ID.SessionID(contains(match_ID.StudyID,Participants(x))==1 & contains(match_ID.TimePoint,T)==1) = temp;
     end
 end
 
-VEPsubject_data.filename=filename;
 
-clear temp x filename
-
-% Find and select patient IDs and sessions with existing matlab file
-vep_files_loc=[];
-no_vep_files_loc=[];
-for x=1:size(VEPsubject_data,1)
-    temp_file=[filepath table2array(VEPsubject_data(x,216))];
-    vep_files_loc=cat(1,vep_files_loc,x);
-
+for x = 1:height(match_ID)
+    match_ID.Filename(x) = {['f00000' num2str(match_ID.SessionID(x)) '_0000' num2str(match_ID.TrialID(x)) '.m']};
 end
 
-clear temp x
+clear temp x y
 
-vep_files=VEPsubject_data(vep_files_loc,:);
-vep_loc=vep_loc(vep_files_loc,:);
-no_vep_filenames=VEPsubject_data(no_vep_files_loc,216);
+%% Extract VEP files
+match_ID.raw_vep = cell(height(match_ID),1);
+for x=1:height(match_ID)
+
+    run([filepath '/' char(match_ID.Filename(x))]);
+
+    match_ID.raw_vep{x} = cat(1,RawDataFrame_1,RawDataFrame_2,RawDataFrame_3,RawDataFrame_4,RawDataFrame_5,RawDataFrame_6,...
+        RawDataFrame_7,RawDataFrame_8,RawDataFrame_9,RawDataFrame_10,RawDataFrame_11,RawDataFrame_12,RawDataFrame_13,...
+        RawDataFrame_14,RawDataFrame_15);
+
+    clear Raw*
+end
 
 
-% Extract VEP files
-for x=1:size(vep_files,1)
-    if vep_loc(x,:)==['Ship']
-        run([filepath_ship table2array(vep_files(x,216))]);
-    else if vep_loc(x,:)==['Clin']
-            run([filepath_clinic table2array(vep_files(x,216))]);
-        end
+%% Filter and parse VEP
+
+Fs = 1024; % Sampling rate
+reversal = 2; % number of reversals
+d1 = designfilt('bandstopiir','FilterOrder',20,'HalfPowerFrequency1',59,...
+    'HalfPowerFrequency2',61,'SampleRate',Fs);  % Bandstop filter for 60Hz powerline noise in VEP signal
+d2 = designfilt('bandstopiir','FilterOrder',20,'HalfPowerFrequency1',119,...
+        'HalfPowerFrequency2',121,'SampleRate',Fs); % Bandstop filter for 120Hz noise harmonic in VEP signal
+
+discarded_trials = 0;
+total_trials = 0;
+
+match_ID.x_data = cell(height(match_ID),1);
+match_ID.cleaned_vep = cell(height(match_ID),1);
+
+for x=1:height(match_ID)
+    VEP=match_ID.raw_vep{x};
+ 
+    dur = length(VEP)./Fs; % total duration of recording (in seconds)
+    match_ID.x_data{x} = 1/Fs:1/Fs:1/reversal;
+
+    VEP = filter(d1,VEP);
+    VEP = filter(d2,VEP);
+    
+
+    y_data=reshape(VEP,[Fs/reversal,dur*reversal]);
+
+    % remove trials where the absolute max voltage >1
+    temp=find(max(abs(y_data),[],1)<1);
+    temp2=find(max(abs(y_data),[],1)>=1);
+    figure(10)
+    plot(y_data(:,temp2))
+    title('bad trial')
+    pause
+    bad_trials = size(y_data,2)-length(temp);
+    discarded_trials = discarded_trials+bad_trials;
+    total_trials = total_trials+size(y_data,2);
+    disp(['number of discarded trials =' num2str(bad_trials)]);
+    y_data=y_data(:,temp)';
+    
+    match_ID.cleaned_vep{x} = y_data;
+    
+    clear x_data y_data
+end
+
+disp(['proportion of discarded trials =' num2str(discarded_trials./total_trials)]);
+
+
+%% Organize data by participant and session
+
+
+
+% Normalize data
+for x=1:length(cleaned_vep)
+    y_data=cell2mat(cleaned_vep(x,4));
+    
+    
+    % Normalize pre-response to 0
+    for y=1:size(y_data,1)
+        temp=mean(mean(y_data(y,1:51)));
+        y_data(y,:)=y_data(y,:)-(temp*ones(size(y_data(y,:))));
     end
-        if exist('RawDataFrame_20')==1
-            all_VEP=cat(1,RawDataFrame_1,RawDataFrame_2,RawDataFrame_3,RawDataFrame_4,RawDataFrame_5,RawDataFrame_6,...
-                RawDataFrame_7,RawDataFrame_8,RawDataFrame_9,RawDataFrame_10,RawDataFrame_11,RawDataFrame_12,RawDataFrame_13,...
-                RawDataFrame_14,RawDataFrame_15,RawDataFrame_16,RawDataFrame_17,RawDataFrame_18,RawDataFrame_19,RawDataFrame_20);
-        else if exist('RawDataFrame_20')==0
-                all_VEP=cat(1,RawDataFrame_1,RawDataFrame_2,RawDataFrame_3,RawDataFrame_4,RawDataFrame_5,RawDataFrame_6,...
-                    RawDataFrame_7,RawDataFrame_8,RawDataFrame_9,RawDataFrame_10,RawDataFrame_11,RawDataFrame_12,RawDataFrame_13,...
-                    RawDataFrame_14,RawDataFrame_15);
-            end
-        end
-        clear A Raw*
-        
-        raw_vep{x,1}=vep_files(x,:).uniqueID;
-        raw_vep{x,2}=vep_files(x,:).age_vep;
-        raw_vep{x,3}=all_VEP;
+    
+    cleaned_vep{x,4}=y_data;
 end
 
-% save VEP_and_subject_data VEPsubject_data vep_files raw_vep
 
-clear
+clear *temp
+
+
+save([filepath '/raw_vep_files.mat'],'match_ID')
